bb38767c69f5aa3f23efdfe41c19305e9e118069
Jamie Morton <jamietmorton@gmail.com>
2019-09-24 14:16:24 -0400

    updating with most recent version of jpgeneviz

--- data/edges/lung-cancer/edges.sh
+++
@@ -1,6 +0,0 @@
-from mmvec.util import rank_hits
-import pandas as pd
-ranks = pd.read_csv('ranks.csv', index_col=0)
-edges = rank_hits(ranks, k=5)
-edges.head()
-edges.to_csv('edges_k5.txt', sep='\t')
